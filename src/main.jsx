8f401a6665a15c92d3219dde20552076dcd415ad
meddie-dev <johnmeddiemariano@gmail.com>
2024-06-30 14:22:01 +0800

    Index: Image Path Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import './index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/weather/">
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
 
